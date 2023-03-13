de59374eccbc90f14d28552095328887a88c8b27
Krisser12311 <Kristoffer6670@gmail.com>
2023-03-13 21:34:59 +0100

    Kinda stuck, I don't know how i'm going to handle internal features from app lab such as OnEvent()

--- yearOne/programing/python/webscraper/main.py
+++ yearOne/programing/python/webscraper/main.py
@@ -2,10 +2,33 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# Notet: This variavble is used to set the path of the webdriver
+url = "https://studio.code.org/projects/applab/AuKpIVK2yflFAc8rpeXJdx8FjY39o2nOZIbynKS3KTE"
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://www.google.com")
+driver.get(url)
+print(driver.title)
+driver.implicitly_wait(1)
 
-title = driver.title
-print(title)
+# Gets the main element called visualization 
+visualization = driver.find_element(By.ID, "visualization")
+
+print(visualization)
+
+#def GrabScreenShot(): # This function is used to grab a screenshot of each page
+#    driver.save_screenshot("screenshot.png") # This is the name of the screenshot TODO: Make this dynamic
+
+def HTMLGrab(): # This function is used to grab the element inside of the visualization container and save it as a html file
+    html = visualization.get_attribute('innerHTML')
+    
+
+def CSSGrab(): # This function is used to grab the element inside of the visualization container and save it as a css file TODO: make it dynamic so it can be used for other elements as well
+    #css = visualization.get_attribute('style')
+    print("Success")
+
+def JSGrab(): # This function is used to grab the element inside of the visualization container and save it as a js file TODO: make it dynamic so it can be used for other elements as well
+    #js = visualization.get_attribute('script')
+    print("Success")
